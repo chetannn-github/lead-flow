586a6adef6462efbf668653bb9a80bd003c471d0
chetannn-github <chetan.rajawat25@gmail.com>
2026-05-08 19:32:02 +0530

    feat - get,update and delete leads routes

--- lead-service/src/controllers/lead.controller.js
+++ lead-service/src/controllers/lead.controller.js
@@ -59,3 +59,134 @@ export const createLead = async (req, res) => {
         });
     }
 };
+
+
+export const getLeads = async (req, res) => {
+    try {
+        const { search, status, today } = req.query;
+        let query = { userId: req.user.userId };
+
+        if (status) query.status = status;
+
+        if (search) {
+            query.$or = [
+                { fullName: { $regex: search, $options: 'i' } },
+                { company: { $regex: search, $options: 'i' } }
+            ];
+        }
+
+        if (today === 'true') {
+            const startOfDay = new Date();
+            startOfDay.setHours(0, 0, 0, 0);
+
+            const endOfDay = new Date();
+            endOfDay.setHours(23, 59, 59, 999);
+
+            query.nextFollowUp = {
+                $gte: startOfDay,
+                $lte: endOfDay
+            };
+        }
+
+        const leads = await Lead.find(query).sort({ createdAt: -1 });
+
+        return res.status(200).json({
+            success: true,
+            count: leads.length,
+            data: leads
+        });
+
+    } catch (error) {
+        console.error(`[LeadService][GetLeads][Error]: ${error.message}`);
+        return res.status(500).json({
+            success: false,
+            error: "INTERNAL_SERVER_ERROR",
+            message: "Failed to retrieve leads."
+        });
+    }
+};
+
+
+export const updateLead = async (req, res) => {
+    try {
+        const { leadId, status, nextFollowUp, newNote } = req.body;
+        const lead = await Lead.findOne({ _id: leadId, userId: req.user.userId });
+
+        if (!lead) {
+            return res.status(404).json({
+                success: false,
+                error: "NOT_FOUND",
+                message: "Lead not found or you don't have permission to edit it."
+            });
+        }
+
+        let updateData = {};
+        if (status) updateData.status = status;
+        if (nextFollowUp) updateData.nextFollowUp = nextFollowUp;
+
+        let updateQuery = { $set: updateData };
+        
+        if (newNote && newNote.trim() !== "") {
+            updateQuery.$push = { 
+                notes: { 
+                    description: newNote, 
+                    date: new Date() 
+                } 
+            };
+        }
+
+        const updatedLead = await Lead.findByIdAndUpdate(
+            id,
+            updateQuery,
+            { new: true, runValidators: true }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Lead updated successfully",
+            data: updatedLead
+        });
+
+    } catch (error) {
+        console.error(`[LeadService][UpdateLead][Error]: ${error.message}`);
+        return res.status(500).json({
+            success: false,
+            error: "INTERNAL_SERVER_ERROR",
+            message: "Failed to update lead details."
+        });
+    }
+};
+
+
+export const deleteLead = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedLead = await Lead.findOneAndDelete({ 
+            _id: id, 
+            userId: req.user.userId 
+        });
+
+        if (!deletedLead) {
+            return res.status(404).json({
+                success: false,
+                error: "NOT_FOUND",
+                message: "Lead not found or you are not authorized to delete it."
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Lead deleted successfully",
+            deletedId: id
+        });
+
+    } catch (error) {
+        console.error(`[LeadService][DeleteLead][Error]: ${error.message}`);
+
+        return res.status(500).json({
+            success: false,
+            error: "INTERNAL_SERVER_ERROR",
+            message: "An error occurred while trying to delete the lead."
+        });
+    }
+};
--- lead-service/src/routes/lead.route.js
+++ lead-service/src/routes/lead.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { createLead } from '../controllers/lead.controller.js';
+import { createLead, deleteLead, getLeads, updateLead } from '../controllers/lead.controller.js';
 const router = express.Router();
 
 router.post('/', createLead);
+router.get('/', getLeads);
+router.patch('/', updateLead);
+router.delete('/', deleteLead);
 
 export default router;
